03d38d9815f04729d8dd5fd352cf5fa92fe41e51
Serenity <sarah.k.hopper@gmail.com>
2023-01-16 21:34:36 -0800

    update inquirer code

--- index.js
+++ index.js
@@ -8,32 +8,79 @@ inquirer
         {
             type: 'input',
             message: 'Please enter team manager name',
-            name: 'mgr-name',
+            name: 'mgrName',
         },
         {
-            type: 'input',
+            type: 'number',
             message: 'Please enter team manager employee ID',
-            name: 'mgr-id',
+            name: 'mgrId',
         },
         {
             type: 'input',
             message: 'Please enter team manager email address',
-            name: 'mgr-email',
+            name: 'mgrEmail',
         },
         {
-            type: 'input',
+            type: 'number',
             message: 'Please enter team manager office number',
-            name: 'mgr-office',
-        }
-    ])
-    //process/parse data to print console log reiterating what user has entered and prompting user to accept or redo. If accept, user is presented with menu and can either add employee or engineer.
-    //data validation needed for mgr info?
-        //TODO: look up inquirer syntax for above fxnality
+            name: 'mgrOffice',
+        },
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: ['Add an engineer to my team', 'Add an intern to my team', new inquirer.Separator(), 'Quit'],
+            name: 'action',
+        },
+    ]);
 
-    .then((data)) => {
+    //save user input for team mgr info into json file for info verification purposes?
+    // .then((data) => {
+        // const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
         
-    }
+        // fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+        //     err ? console.log(err) : console.log('Successfully saved team manager information.')
+        //     );
+        
+        // console.log("Hello, " + `${data.name}` + ".");
+
+        //TODO: present user with menu allowing them to add an employee or intern to their team.
+            //look up inquirer syntax for this... user must have entered team manager info to get to menu
+
+    //     prompt([
+    //         {
+    //             type: 'list',
+    //             message: 'What would you like to do?',
+    //             choices: ['Add an engineer to my team', 'Add an intern to my team', new inquirer.Separator(), 'Quit'],
+    //             name: 'action',
+    //         }
+    //     ]
+
+    //     )
+
+    // });
+
+    // inquirer
+    //     .prompt.next([
+    //         {
+    //             type: 'list',
+    //             message: 'What would you like to do?',
+    //             choices: ['Add an engineer to my team', 'Add an intern to my team', new inquirer.Separator(), 'Quit'],
+    //             name: 'action',
+    //         }
+    //     ])
+    //         .then((answer) => {
+    //             if (`${answer.action}` === 'Add an engineer to my team') {
+    //                 inquirer.prompt([])
+    //             } else if (`${answer.action}` === 'Add an intern to my team') {
+    //                 inquirer.prompt([])
+    //             } else {
+    //                 console.log("Goodbye")
+    //             }
+                    
+    //         })
+            
 
 //TODO: write user input to index.html file (check activities for fs read and write fxns, make sure updates are persistant and not overwriting past changes)
+    //TODO: also check activities for past example of generating html dynamically using template literals
 
 //TODO: deploy to heroku?
