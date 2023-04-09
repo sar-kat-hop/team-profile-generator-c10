86a7ba85aecfacaa8cbcdccf94c5c607c511bbc0
Sarah <sarah.k.hopper@gmail.com>
2023-04-08 21:25:51 -0700

    fix inquirer code for basic functionality

--- index.js
+++ index.js
@@ -3,94 +3,191 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-//refactor using async
+
+
 async function start() {
-    const answer = await inquirer.prompt(
+    const answer = await inquirer.prompt({
+        type: 'list',
+        name: 'menu',
+        message: 'What would you like to do?',
+        choices: [
+        { name: 'Add a new engineer to my team', value: 'addEngi' },
+        { name: 'Add a new intern to my team', value: 'addIntern' },
+        { name: 'View my team', value: 'viewTeam' },
+        { name: 'Quit', value: 'quit'},
+        ],
+    });
+
+    if (answer.menu === 'addEngi') {
+        // ask questions for option 1
+        const menuEngi = await inquirer.prompt([
         {
-            type: 'list',
-            name: 'userAction',
-            message: 'What would you like to do?',
-            choices: [
-                { name: 'addEngi', value: 'Add an engineer to my team'},
-                { name: 'addIntern', value: 'Add an intern to my team'},
-                { name: 'viewTeam', value: 'View my team'},
-                { name: 'exit', value: 'Quit'},
-            ],
-        });
-
-    if (answer.option === 'addEngi') {
-        const newEngi = await inquirer.prompt([
-            {
-                type: 'input',
-                message: "Please enter the engineer's name",
-                name: 'engiName',
-            },
-            {
-                type: 'input',
-                message: "Please enter the engineer's email address",
-                name: 'engiEmail',
-            },
-            {
-                type: 'number',
-                message: "Please enter the engineer's GitHub username",
-            },
+            type: 'input',
+            name: 'engiName',
+            message: "Enter the engineer's name:",
+        },
+        {
+            type: 'input',
+            name: 'engiEmail',
+            message: "Enter the engineer's email:",
+        },
+        {
+            type: 'input',
+            name: 'engiGit',
+            message: "Enter the engineer's GitHub portfolio URL:"
+        }
         ]);
-        console.log("New engineer added: " + newEngi);
-        //wait until action is performed before asking if user wants to go back
-        await showReturnMsg();
+        console.log(`New engineer added: ${menuEngi.engiName}`);
+        await showReturnMessage();
 
-    } else if (answer.option === 'addIntern') {
-        const newIntern = await inquirer.prompt([
-            {
-                type: 'input',
-                message: "Please enter the intern's name",
-                name: 'internName',
-            },
-            {
-                type: 'number',
-                message: "Please enter the intern's ID number",
-                name: 'internId',
-            },
-            {
-                type: 'input',
-                message: "Please enter the intern's email address",
-                name: 'internEmail',
-            },
-            {
-                type: 'number',
-                message: "Please enter the intern's school",
-                name: 'internSchool',
-            },
+    } else if (answer.menu === 'addIntern') {
+        // ask questions for option 2
+        const menuIntern = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'internName',
+            message: "Enter the intern's name:",
+        },
         ]);
-        console.log("New intern added: " + newIntern);
-        await showReturnMsg();
+        console.log(`New intern added: ${menuIntern.internName}`);
+        await showReturnMessage();
 
-    } else if (answer.option === 'viewTeam') {
-
+    } else if (answer.menu === 'viewTeam') {
+        // ask questions for option 3
+        const menuTeam = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'seeTeam',
+            message: 'Here is your team:',
+        },
+        ]);
+        console.log(`${menuTeam.seeTeam}`);
+        await showReturnMessage();
     }
 }
 
-async function showReturnMsg() {
+async function showReturnMessage() {
     const bottomBar = new inquirer.ui.BottomBar();
-    bottomBar.updateBottomBar("Press any key to continue or 'm' to return to the menu.");
-    const keyPress = await keypress();
 
-    if (keyPress === "m") {
-        start();
-    } else {
-        bottomBar.updateBottomBar("");
+    while (true) {
+        bottomBar.updateBottomBar(
+            "Press 'm' to return to the menu or 'esc' to quit."
+        );
+        
+        const keyPress = await keypress();
+
+        if (keyPress === 'm') {
+            return start();
+
+        } else if (keyPress === '\u001b') { // check for "escape" key press
+            process.exit(); // exit the app
+        }
+        // if the user presses a key other than "m" or "Esc"
+        bottomBar.updateBottomBar('Ivalid key pressed. Please press "m" to return to the menu or "esc" to quit.');
+        }
     }
-}
 
 function keypress() {
     return new Promise((resolve) => {
-        process.stdin.once("data", (data) => {
+        process.stdin.once('data', (data) => {
             resolve(data.toString().trim());
         });
     });
 }
 
 start();
+
+
+//refactor using async
+// async function start() {
+//     const answer = await inquirer.prompt({
+//             type: 'list',
+//             name: 'userAction',
+//             message: 'What would you like to do?',
+//             choices: [
+//                 { name: 'Add an engineer to my team', value: 'addEngi'},
+//                 { name: 'Add an intern to my team', value: 'addIntern'},
+//                 { name: 'View my team', value: 'viewTeam'},
+//                 { name: 'exit', value: 'Quit'},
+//             ],
+//         });
+
+//     if (answer.value === 'addEngi') {
+//         const newEngi = await inquirer.prompt([
+//             {
+//                 type: 'input',
+//                 message: "Please enter the engineer's name",
+//                 name: 'engiName',
+//             },
+//             {
+//                 type: 'input',
+//                 message: "Please enter the engineer's email address",
+//                 name: 'engiEmail',
+//             },
+//             {
+//                 type: 'number',
+//                 message: "Please enter the engineer's GitHub username",
+//             },
+//         ]);
+//         console.log("New engineer added: " + newEngi);
+//         //wait until action is performed before asking if user wants to go back
+//         await showReturnMsg();
+
+//     } else if (answer.value === 'addIntern') {
+//         const newIntern = await inquirer.prompt([
+//             {
+//                 type: 'input',
+//                 message: "Please enter the intern's name",
+//                 name: 'internName',
+//             },
+//             {
+//                 type: 'number',
+//                 message: "Please enter the intern's ID number",
+//                 name: 'internId',
+//             },
+//             {
+//                 type: 'input',
+//                 message: "Please enter the intern's email address",
+//                 name: 'internEmail',
+//             },
+//             {
+//                 type: 'number',
+//                 message: "Please enter the intern's school",
+//                 name: 'internSchool',
+//             },
+//         ]);
+//         console.log("New intern added: " + newIntern);
+//         await showReturnMsg();
+
+//     } else if (answer.value === 'viewTeam') {
+
+//     }
+// }
+
+// async function showReturnMsg() {
+//     const bottomBar = new inquirer.ui.BottomBar();
+//     bottomBar.updateBottomBar("Press 'm' to return to the menu or 'esc' to quit.");
+//     const keyPress = await keypress();
+
+//     if (keyPress === "m") {
+//         start();
+//     } else if (keyPress === "\u001b") {
+//         bottomBar.updateBottomBar("Goodbye.");
+//         process.exit();
+//     } else {
+//         bottomBar.updateBottomBar("Invalid key pressed. Please press 'm' to go back to the menu or 'esc' to quit.");
+//     }
+// };
+
+// function keypress() {
+//     return new Promise((resolve) => {
+//         process.stdin.once("data", (data) => {
+//             resolve(data.toString().trim());
+//         });
+//     });
+// };
+
+// start();
 
 // inquirer.prompt([
 //         {
