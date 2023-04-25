b6a1b6774713d60b536adfd656d6cb13403acc26
Sarah <sarah.k.hopper@gmail.com>
2023-04-24 20:31:58 -0700

    successful refactor of fs.writeFileSync, minus formatting weirdness

--- src/templateHelper.js
+++ src/templateHelper.js
@@ -12,81 +12,156 @@ function renderPage(teamMembers) {
     // const employeeCard = teamMembers.map((employee) => {
         // return `
 
-    teamMembers.forEach(employee => {
-        let employeeCard = `
-        <div class="card border m-3" style="height: 250px; width: 275px">
-        <div class="card-header text-center p-3 bg-info text-white fs-5">
-            ${employee.getRole()}
-        </div>
-
-        <div class="card-body">
-            <p class="fw-light fst-italic lh-1 text-wrap text-center p-0">
-                ${employee.name}
-            </p>
-            <ul class="fw-light">
-                <li class="lw-light lh-1 text-wrap p-0"> <b>ID: </b>
-                    ${employee.id}
-                </li>
-                <li class="lw-light lh-1 text-wrap p-0"> <b>Email: </b>
-                    <a href="mailto:Email">${employee.email}</a></li>
-            `;
-
-            if (employee instanceof Manager) {
-                employeeCard += `
-                <li class='lw-light lh-1 text-wrap p-0'> Office No: ${employee.getSpecial()}</li></ul>
-                `;
-
-            } else if (employee instanceof Engineer) {
-                employeeCard += `
-                <li class='lw-light lh-1 text-wrap p-0'> GitHub: <a href='https://github.com/${employee.getSpecial()}'>${employee.getSpecial()}</a></li></ul>
-                `;
-
-            } else if (employee instanceof Intern) {
-                employeeCard += `
-                <li class='lw-light lh-1 text-wrap p-0'> School: ${employee.getSpecial()}</li></ul>
-                `;
-            }
-
-            employeeCard += `
+    // const renderHeader = () => {
+    const header = `
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+                <title>TeamBuilder</title>
+            </head>
+    
+            <body class="bg-light">
+                <header class="bg-info p-4">
+                    <div class="container-fluid text-center text-white">
+                        <h1>TeamBuilder</h1>
+                    </div>
+                </header>
+    
+                <div class="container-fluid">
+                    <h2 class="text-center fs-3 m-4 fst-italic">My Team</h2>
                 </div>
-            </div>
+
+                <div class="d-flex flex-wrap justify-content-center bd-highlight m-5">
+        `;
+
+        // fs.writeFileSync('index.html', header, { flag: 'w'});
+    // };
+
+    const employeeCards = teamMembers.map(employee => {
+
+        let specialInfo = '';
+
+        if (employee instanceof Manager) {
+            // employeeCard += `
+            specialInfo = `
+            <li class='lw-light lh-1 text-wrap p-0'> Office No: ${employee.getSpecial()}</li></ul>
             `;
 
-            const htmlPg = 
-            `
-            <!DOCTYPE html>
-            <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-                    <title>TeamBuilder</title>
-                </head>
+        } else if (employee instanceof Engineer) {
+            // employeeCard += `
+            specialInfo = `
+            <li class='lw-light lh-1 text-wrap p-0'> GitHub: <a href='https://github.com/${employee.getSpecial()}'>${employee.getSpecial()}</a></li></ul>
+            `;
+
+        } else if (employee instanceof Intern) {
+            // employeeCard += `
+            specialInfo = `
+            <li class='lw-light lh-1 text-wrap p-0'> School: ${employee.getSpecial()}</li></ul>
+            `;
+        }
         
-                <body class="bg-light">
-                    <header class="bg-info p-4">
-                        <div class="container-fluid text-center text-white">
-                            <h1>TeamBuilder</h1>
-                        </div>
-                    </header>
+        return `
+        <div class="card border m-3" style="height: 250px; width: 275px">
+            <div class="card-header text-center p-3 bg-info text-white fs-5">
+                ${employee.getRole()}
+            </div>
+
+            <div class="card-body">
+                <p class="fw-light fst-italic lh-1 text-wrap text-center p-0">
+                    ${employee.name}
+                </p>
+                <ul class="fw-light">
+                    <li class="lw-light lh-1 text-wrap p-0"> <b>ID: </b>
+                        ${employee.id}
+                    </li>
+                    <li class="lw-light lh-1 text-wrap p-0"> <b>Email: </b>
+                        <a href="mailto:Email">${employee.email}</a></li>
+                    ${specialInfo}
+                `;
+    }).join('');
+
+    const htmlPage = header + employeeCards + '</div></div></body></html>';
+
+    fs.writeFileSync('myteam.html', htmlPage);
+
+    // teamMembers.forEach(employee => {
+    //     let employeeCard = `
+    //     <div class="card border m-3" style="height: 250px; width: 275px">
+    //     <div class="card-header text-center p-3 bg-info text-white fs-5">
+    //         ${employee.getRole()}
+    //     </div>
+
+    //     <div class="card-body">
+    //         <p class="fw-light fst-italic lh-1 text-wrap text-center p-0">
+    //             ${employee.name}
+    //         </p>
+    //         <ul class="fw-light">
+    //             <li class="lw-light lh-1 text-wrap p-0"> <b>ID: </b>
+    //                 ${employee.id}
+    //             </li>
+    //             <li class="lw-light lh-1 text-wrap p-0"> <b>Email: </b>
+    //                 <a href="mailto:Email">${employee.email}</a></li>
+    //         `;
+
+    //         if (employee instanceof Manager) {
+    //             employeeCard += `
+    //             <li class='lw-light lh-1 text-wrap p-0'> Office No: ${employee.getSpecial()}</li></ul>
+    //             `;
+
+    //         } else if (employee instanceof Engineer) {
+    //             employeeCard += `
+    //             <li class='lw-light lh-1 text-wrap p-0'> GitHub: <a href='https://github.com/${employee.getSpecial()}'>${employee.getSpecial()}</a></li></ul>
+    //             `;
+
+    //         } else if (employee instanceof Intern) {
+    //             employeeCard += `
+    //             <li class='lw-light lh-1 text-wrap p-0'> School: ${employee.getSpecial()}</li></ul>
+    //             `;
+    //         }
+
+    //         employeeCard += `
+    //             </div>
+    //         </div>
+    //         `;
+
+    //         // const htmlPg = 
+    //         // `
+    //         // <!DOCTYPE html>
+    //         // <html lang="en">
+    //         //     <head>
+    //         //         <meta charset="UTF-8">
+    //         //         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    //         //         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    //         //         <title>TeamBuilder</title>
+    //         //     </head>
         
-                    <div class="container-fluid">
-                        <h2 class="text-center fs-3 m-4 fst-italic">My Team</h2>
-                    </div>
+    //         //     <body class="bg-light">
+    //         //         <header class="bg-info p-4">
+    //         //             <div class="container-fluid text-center text-white">
+    //         //                 <h1>TeamBuilder</h1>
+    //         //             </div>
+    //         //         </header>
         
-                    <div class="d-flex flex-wrap justify-content-center bd-highlight m-5">
-                        ${employeeCard}
-                    </div>
+    //         //         <div class="container-fluid">
+    //         //             <h2 class="text-center fs-3 m-4 fst-italic">My Team</h2>
+    //         //         </div>
         
-                </body>
-                <footer></footer>
-            </html>
-            `;        
+    //         //         <div class="d-flex flex-wrap justify-content-center bd-highlight m-5">
+    //         //             ${employeeCard}
+    //         //         </div>
+        
+    //         //     </body>
+    //         //     <footer></footer>
+    //         // </html>
+    //         // `;        
             
-            const fileName = './index.html';
-            fs.writeFileSync(fileName, htmlPg, {flag: 'a'});
-                console.log('Employee info written to index.html!');
-        });
+    //         const fileName = './index.html';
+    //         fs.writeFileSync(fileName, htmlPg, {flag: 'a'});
+    //             console.log('Employee info written to index.html!');
+    //     });
 }
 
 module.exports = renderPage;
